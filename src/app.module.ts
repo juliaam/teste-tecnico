d1a7138df4aaafc4f37a73c2988e6e5290f7fd80
juliaam <juliademoraess@gmail.com>
2024-05-09 22:51:20 -0300

    fix: menu module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './modules/product/product.module';
 import { CategoryModule } from './modules/category/category.module';
+import { MenuModule } from './modules/menu/menu.module';
 
 @Module({
-  imports: [ProductModule, CategoryModule],
+  imports: [ProductModule, CategoryModule, MenuModule],
   controllers: [],
   providers: [],
 })
