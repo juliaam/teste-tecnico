bb6c2b80f45206757e05c76e7e62a32b99b325c2
juliaam <juliademoraess@gmail.com>
2024-05-11 19:53:08 -0300

    feat: prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from './modules/product/product.module';
 import { CategoryModule } from './modules/category/category.module';
 import { MenuModule } from './modules/menu/menu.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProductModule, CategoryModule, MenuModule],
+  imports: [ProductModule, CategoryModule, MenuModule, PrismaModule],
   controllers: [],
   providers: [],
 })
--- src/modules/menu/menu.module.ts
+++ src/modules/menu/menu.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MenuService } from './menu.service';
 import { MenuController } from './menu.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
-
 @Module({
   controllers: [MenuController],
-  providers: [MenuService, PrismaService],
+  providers: [MenuService],
 })
 export class MenuModule {}
